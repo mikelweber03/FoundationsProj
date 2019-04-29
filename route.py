0c64f4f1ba44b013833f37eebd2a273abcb8ca02
Mikel Weber <mikelweber03@gmail.com>
2019-04-29 10:35:32 +0200

    Change

--- route.py
+++ route.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template
+import os
+
+from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def home():
 	return render_template("index.html")
+	
 if __name__ == '__main__':
 	app.run()
